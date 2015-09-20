32a6ea7f9c672ec035b0757e3039402600ceaac0
un5t0ppab13 <devbrights@gmail.com>
2015-09-20 14:42:57 -0700

    rspec spec/features/home_spec.rb, add home controller, and root_path

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "visiting the homepage" do
+  scenario "the visitor sees welcome text" do
+    visit root_path
+    expect(page).to have_text("Welcome to railsTemplate")
+  end
+end
+
+# rspec spec/features/home_spec.rb
